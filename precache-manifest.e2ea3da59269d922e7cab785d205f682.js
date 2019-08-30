50dd0c0297cdc846e00955958b2e7b163bf126ab
Vikrant <tnarkiv17@gmail.com>
2019-08-30 20:09:49 +0530

    Updates

---
+++ precache-manifest.e2ea3da59269d922e7cab785d205f682.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b76563b5c6c7ef54d60ba9d23069d794",
+    "url": "/jumble-bumble/index.html"
+  },
+  {
+    "revision": "8c3b75f8f0680b4cd191",
+    "url": "/jumble-bumble/static/css/main.714106d7.chunk.css"
+  },
+  {
+    "revision": "1551eaf1bdcacd36be02",
+    "url": "/jumble-bumble/static/js/2.39e50043.chunk.js"
+  },
+  {
+    "revision": "8c3b75f8f0680b4cd191",
+    "url": "/jumble-bumble/static/js/main.27093fdf.chunk.js"
+  },
+  {
+    "revision": "31a54fb0704a772d88dc",
+    "url": "/jumble-bumble/static/js/runtime~main.18ee6cca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jumble-bumble/static/media/logo.5d5d9eef.svg"
+  }
+]);
